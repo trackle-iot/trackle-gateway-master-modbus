e261d05f29bdef6d7c476c176f002fb3f95d8239
Federico Bombardieri <bombardieri.federico@gmail.com>
2023-04-27 09:57:10 +0200

    feat: first commit

---
+++ include/trackle-gateway-master-modbus.h
@@ -0,0 +1,6 @@
+#ifndef GW_MASTER_MODBUS_H_
+#define GW_MASTER_MODBUS_H_
+
+void GwMasterModbus_hello();
+
+#endif
---
+++ src/trackle-gateway-master-modbus.c
@@ -0,0 +1,8 @@
+#include "trackle-gateway-master-modbus.h"
+
+#include <esp_log.h>
+
+void GwMasterModbus_hello()
+{
+    ESP_LOGE("hello", "Hello World!");
+}
