6fe0f563b181947d34e4542cd3aaaba5f48f07b4
Federico Bombardieri <bombardieri.federico@gmail.com>
2023-04-27 16:56:54 +0200

    chore: updated main header file

--- include/trackle-gateway-master-modbus.h
+++ include/trackle-gateway-master-modbus.h
@@ -3,7 +3,7 @@
 
 #include <driver/uart.h>
 
-#define TRACKLE_GATEWAY_MASTER_MODBUS_VERSION "0.1.0"
+#define TRACKLE_GATEWAY_MASTER_MODBUS_VERSION "0.2.0"
 
 void GwMasterModbus_init(uart_port_t uartPort, int txPin, int rxPin, bool usesRs485, int dirPin);
 void GwMasterModbus_stop();
