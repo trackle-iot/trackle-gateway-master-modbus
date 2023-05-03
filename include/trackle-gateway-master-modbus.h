a2274b6419e50a20955814d09d50853551b7f81f
Federico Bombardieri <bombardieri.federico@gmail.com>
2023-05-03 12:16:30 +0200

    chore: updated version constant

--- include/trackle-gateway-master-modbus.h
+++ include/trackle-gateway-master-modbus.h
@@ -3,7 +3,7 @@
 
 #include <driver/uart.h>
 
-#define TRACKLE_GATEWAY_MASTER_MODBUS_VERSION "1.0.0"
+#define TRACKLE_GATEWAY_MASTER_MODBUS_VERSION "1.0.1"
 
 void GwMasterModbus_init(uart_port_t uartPort, int txPin, int rxPin, bool usesRs485, int dirPin);
 void GwMasterModbus_stop();
